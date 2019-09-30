be458698705ea8021bf826f69eb36de384af362e
jjordan4 <54901599+jjordan4@users.noreply.github.com>
2019-09-30 09:43:17 -0600

    SQL Practice

---
+++ DDL/DDL-Practice.sql
@@ -0,0 +1,78 @@
+/* **********************************************
+ * Simple Table Creation - Columns and Primary Keys
+ *
+ * File: DDL-Practice.sql
+ * Emergency Service & Product
+ * Specification Document 1
+ * Version 1.0.0
+ *
+ * Author: Dan Gilleland
+ ********************************************** */
+-- Select the CREATE DATABASE stement below to create the demo database.
+-- CREATE DATABASE [ESP-A01]
+USE [ESP-A01] -- this is a statement that tells us to switch to a particular database
+-- Notice in the database name above, it is "wrapped" in square brackets because 
+-- the name had a hypen in it. 
+-- For all our other objects (tables, columns, etc), we won't use hypens or spaces, so
+-- the use of square brackets are optional.
+GO  -- this statement helps to "separate" various DDL statements in our script
+    -- so that they are executed as "blocks" of code.
+
+    -- TIP: Press [ctrl] + [shift] + r to refresh script knowledge
+
+    -- To create a database table, we use the CREATE TABLE statement.
+-- Note that the order in which we create/drop tables is important
+-- because of how the tables are related via Foreign Keys.
+/* DROP TABLE statements (to "clean up" the database for re-creation)  */
+/*   You should drop tables in the REVERSE order in which you created them */
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Orders')
+    DROP TABLE Orders
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Customers')
+    DROP TABLE Customers
+
+-- To create a database table, we use the CREATE TABLE statement.
+-- Note that the order in which we create/drop tables is important
+-- because of how the tables are related via Foreign Keys.
+CREATE TABLE Customers
+(
+    -- The body of a CREATE TABLE will identify a comma-separated list of
+    -- Column Declarations and Table Constraints.
+    CustomerNumber  int                 NOT NULL, -- NOT NULL means the data is required
+    FirstName       varchar(50)         NOT NULL,
+    LastName        varchar(60)         NOT NULL,
+    [Address]       varchar(40)         NOT NULL,
+    City            varchar(35)         NOT NULL,
+    Province        char(2)             NOT NULL,
+    PostalCode      char(6)             NOT NULL,
+    PhoneNumber     char(13)                NULL  -- NULL means the data is optional
+)
+
+CREATE TABLE Orders
+(
+    OrderNumber     int                 NOT NULL,
+    CustomerNumber  int                 NOT NULL,
+    [Date]          datetime            NOT NULL,
+    Subtotal        money               NOT NULL,
+    GST             money               NOT NULL,
+    Total           money               NOT NULL
+)
+
+CREATE TABLE InventoryItems
+(
+    ItemNumber          varchar(5)          NOT NULL,
+    ItemDescription     varchar(50)             NULL,
+    CurrentSalePrice    money               NOT NULL,
+    InStockCount        int                 NOT NULL,
+    ReorderLevel        int                 NOT NULL
+)
+
+CREATE TABLE OrderDetails
+(
+    OrderNumber     int                 NOT NULL,
+    ItemNumber      varchar(5)          NOT NULL,
+    Quantity        int                 NOT NULL,
+    SellingPrice    money               NOT NULL,
+    Amount          money               NOT NULL
+)
+
+    /* ==========================Practice SQL Below=========================== */
--- DDL/ReadMe.md
+++ DDL/ReadMe.md
@@ -54,3 +54,6 @@ CHECK (Mark BETWEEN 0 AND 100)
 [Weblink](https://portal.azure.com/?Microsoft_Azure_Education_correlationId=31368EAF3BFF60520441834B3A4C61B0&Microsoft_Azure_Education_newA4E=true#blade/Microsoft_Azure_Education/EducationMenuBlade/software)
 
 Under the options tab in SQL Server managerment, go to text editor, then activate word wrap and show visual glyphs. Under the tab section within all languages; activate insert spaces.
+
+## DROP TABLES
+ 
