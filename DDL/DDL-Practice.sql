d440fe88c099ce0af82035a4040dfe82a5b09aff
jjordan4 <54901599+jjordan4@users.noreply.github.com>
2019-09-30 10:42:57 -0600

    Update DDL-Practice.sql

--- DDL/DDL-Practice.sql
+++ DDL/DDL-Practice.sql
@@ -50,7 +50,17 @@ CREATE TABLE [dbo].[Customers]
 (
     -- The body of a CREATE TABLE will identify a comma-separated list of
     -- Column Declarations and Table Constraints.
-    CustomerNumber  int                 NOT NULL, -- NOT NULL means the data is required
+    CustomerNumber  int
+        -- The following is a PRIMARY KEY constraint that has a specific name
+        -- Primary Key constraints ensure a row of data being added to the table
+        -- will have to have a unique value for the Primary Key column(s)
+        CONSTRAINT PK_Customers_CustomerNumber
+            PRIMARY KEY
+        -- IDENTITY means the database will generate a unique whole-number
+        -- value for this column
+        IDENTITY(100, 1) -- The first number is the "seed",
+                         -- and the last number is the "increment"
+                                        NOT NULL, -- NOT NULL means the data is required
     FirstName       varchar(50)         NOT NULL,
     LastName        varchar(60)         NOT NULL,
     [Address]       varchar(40)         NOT NULL,
@@ -62,8 +72,17 @@ CREATE TABLE [dbo].[Customers]
 
 CREATE TABLE Orders
 (
-    OrderNumber     int                 NOT NULL,
-    CustomerNumber  int                 NOT NULL,
+    OrderNumber     int
+        CONSTRAINT PK_Orders_OrderNumber
+            PRIMARY KEY
+        IDENTITY(200, 1)                NOT NULL,
+    CustomerNumber  int
+        -- Foreign Key constraints ensure that when a row of data is being
+        -- inserted or updated, there is a row in the referenced table
+        -- that has the same value as its Primary Key
+        CONSTRAINT FK_Orders_CustomerNumber_Customers_CustomerNumber
+            FOREIGN KEY REFERENCES
+            Customers(CustomerNumber)   NOT NULL,
     [Date]          datetime            NOT NULL,
     Subtotal        money               NOT NULL,
     GST             money               NOT NULL,
@@ -72,7 +91,9 @@ CREATE TABLE Orders
 
 CREATE TABLE InventoryItems
 (
-    ItemNumber          varchar(5)          NOT NULL,
+    ItemNumber          varchar(5)
+        CONSTRAINT PK_InventoryItems_ItemNumber
+            PRIMARY KEY                     NOT NULL,
     ItemDescription     varchar(50)             NULL,
     CurrentSalePrice    money               NOT NULL,
     InStockCount        int                 NOT NULL,
@@ -81,11 +102,22 @@ CREATE TABLE InventoryItems
 
 CREATE TABLE OrderDetails
 (
-    OrderNumber     int                 NOT NULL,
-    ItemNumber      varchar(5)          NOT NULL,
+    OrderNumber     int
+        CONSTRAINT FK_OrderDetails_OrderNumber_Orders_OrderNumber
+            FOREIGN KEY REFERENCES
+            Orders(OrderNumber)         NOT NULL,
+    ItemNumber      varchar(5)
+        CONSTRAINT FK_OrderDetails_ItemNumber_InventoryItems_ItemNumber
+            FOREIGN KEY REFERENCES
+            InventoryItems(ItemNumber)  NOT NULL,
     Quantity        int                 NOT NULL,
     SellingPrice    money               NOT NULL,
-    Amount          money               NOT NULL
+    Amount          money               NOT NULL,
+    -- The following is a Table Constraint
+    -- A composite primary key MUST be done as a Table Constraint
+    -- because it involves two or more columns
+    CONSTRAINT PK_OrderDetails_OrderNumber_ItemNumber
+        PRIMARY KEY (OrderNumber, ItemNumber) -- Specify all the columns in the PK
 )
 
     /* ==========================Practice SQL Below=========================== */
