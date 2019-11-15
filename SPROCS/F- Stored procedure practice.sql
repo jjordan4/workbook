ec81dcf701d550ff92ddb8dbf5be133421ec23f2
jjordan4 <54901599+jjordan4@users.noreply.github.com>
2019-11-15 09:42:25 -0700

    work

--- SPROCS/F- Stored procedure practice.sql
+++ SPROCS/F- Stored procedure practice.sql
@@ -17,15 +17,57 @@ IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_TYPE = N'PROC
 GO
 CREATE PROCEDURE DissolveClub
     -- Parameters here
-    @ClubID varchar (10),
-    @ClubName varchar (50),
-    @StudentID int 
+    @ClubID varchar (10)
+ 
 AS
     -- Body of procedure here
     -- Basic Validation - Parameter values are required
-
+    --A) Make sure the ClubID is not NULL
+    IF  @ClubID IS NULL 
+    BEGIN
+        RAISERROR('ClubID is required (cannot be null)', 16, 1)
+    END
+    ELSE
+    BEGIN
+    --B) Make sure the Club exists
+    IF NOT EXISTS(SELECT ClubId FROM Club WHERE ClubId = @ClubID)
+    BEGIN
+         RAISERROR('that club does not exist', 16, 1)
+    END
+    ELSE
+    BEGIN
+    --Transaction:
+    BEGIN TRANSACTION -- temporary
+    --1) Remove members of club(from Activity)
+    DELETE FROM Activity WHERE ClubId = @ClubID
+    IF @@ERROR <> 0 -- then there's a problem with the delete
+    BEGIN
+      ROLLBACK TRANSACTION -- reverses the "temporary" changes to the database
+      RAISERROR('Unable to remove members from the club', 16, 1)
+        END
+        ELSE
+        BEGIN
+    --2)Remove the club
+    DELETE FROM Club WHERE ClubId = @ClubID
+         IF @@ERROR <> 0 OR @@ROWCOUNT = 0 -- a problem 
+        BEGIN
+        ROLLBACK TRANSACTION -- reverses the "temporary" changes to the database
+        RAISERROR('Unable to remove members from the club', 16, 1)
+        END
+        ELSE
+        BEGIN
+        COMMIT TRANSACTION -- Finalize all temp DML statments
+    END
+ END
+ END     
+ END      
 RETURN
 GO
+
+EXEC
+
+--SELECT * FROM Activity
+--SELECT * FROM Club
 
 --2. Create a stored procedure called ArchivePayments.This stored procedure must transfer all payment records to the StudentPaymentArchive table. After archiving,delete the payment records.
 IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'StudentPaymentArchive')
